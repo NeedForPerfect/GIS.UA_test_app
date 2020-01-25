a626f0742846641772904717686cb4b0948e9293
Volodymyr Shorinov <volodymyr.shorinov@emotorwerks.com>
2020-01-25 21:49:43 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SuppliersListComponent } from './suppliers-list/suppliers-list.component';
+import { EditSupplierComponent } from './edit-supplier/edit-supplier.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { MenuComponent } from './header/menu/menu.component';
+
+const routes: Routes = [
+  { path: '', component: SuppliersListComponent },
+  { path: 'edit-supplier/:id', component: EditSupplierComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, SuppliersListComponent, EditSupplierComponent, HeaderComponent, LoginComponent, AccessDeniedComponent, MenuComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/edit-supplier/edit-supplier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-supplier',
+  templateUrl: './edit-supplier.component.html',
+  styleUrls: ['./edit-supplier.component.scss']
+})
+export class EditSupplierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
