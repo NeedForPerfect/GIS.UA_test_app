ab119c05690683376a7244047cae850b477f4d61
Volodymyr Shorinov <volodymyr.shorinov@emotorwerks.com>
2020-01-25 17:34:17 +0200

    basic set up ng project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  local_rest_api_server: 'http://localhost:3000',
+  local_websocket_server: 'http://localhost:3001'
 };
 
 /*
